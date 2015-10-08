e753047fd9ca6384d29ff5682a5786f04d3af23a
Fabian996 <blauerdrache2@aol.de>
2015-10-08 19:18:03 +0200

    Update Main.java

--- src/LoginNachricht/Main.java
+++ src/LoginNachricht/Main.java
@@ -1,4 +1,4 @@
-package de.Fabian.LoginNachticht;
+package de.Fabian996.LoginNachticht;
 
 import java.util.logging.Logger;
 
@@ -18,66 +18,84 @@ public class main extends JavaPlugin implements Listener{
 
 	Config settings = Config.getInstance();
 	
+	Updater updater = new Updater(this, 95083, getFile(), Updater.UpdateType.DEFAULT, true);
+	
 	//onEnable
 	public void onEnable(){
 		this.settings.setup(this);
 		Bukkit.getPluginManager().registerEvents(this, this);
 		saveConfig();
 		
+		// Updater
+	    Boolean AutoUpdate = Boolean.valueOf(getConfig().getBoolean("ln.AutoUpdate.Enabled?", false));
+	    if (AutoUpdate.booleanValue())
+	    {
+	      if (Updater.UpdateResult.UPDATE_AVAILABLE != null)
+	        System.out.println("[Login Nachricht] New version avaible!");
+	      else if (Updater.UpdateResult.NO_UPDATE != null)
+	      {
+	        System.out.println("[Login Nachricht]No new version avaible!");
+	      }
+	    }
+	    else {
+	      System.out.println("[Login Nachricht] AutoUpdater for this plugin is currently disabled!");
+	    }
+	    loadConfig();
+	    reloadConfig();
 	
 		// Version
-		PluginDescriptionFile tml = getDescription();
-		Logger tmllog = getLogger();
-		tmllog.info(tml.getName() + " has been enabled (V." + tml.getVersion() + ")");
+	    PluginDescriptionFile tml = getDescription();
+	    Logger tmllog = getLogger();
+	    tmllog.info(tml.getName() + " has been enabled (V." + tml.getVersion() + ")");
 	}
 	
 	//onDisable
 	public void onDisable(){
-		
 		//Version
-		PluginDescriptionFile tml = getDescription();
-		Logger tmllog = getLogger();
-		tmllog.info(tml.getName() + " has been disable (V." + tml.getVersion() + ")");
-		
+	    PluginDescriptionFile tml = getDescription();
+	    Logger tmllog = getLogger();
+	    tmllog.info(tml.getName() + " has been disable (V." + tml.getVersion() + ")");
 	}
 	
 	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent e)
-	{
+	public void onPlayerJoin(PlayerJoinEvent e){
 		Player p = e.getPlayer();
-		if(p.hasPermission("loginachricht.default"))
-		{
-		e.setJoinMessage(ChatColor.AQUA + (this.settings.getConfig().getString("message").replace("%p%", p.getName())));
-		}
+			e.setJoinMessage(ChatColor.AQUA + (this.settings.getConfig().getString("message").replace("%p%", p.getName())));
+		
 	}
 	@EventHandler
-	public void onPlayerLeft(PlayerQuitEvent e)
-	{
+	public void onPlayerLeft(PlayerQuitEvent e){
 		Player p = e.getPlayer();
-		if(p.hasPermission("loginachricht.default"))
-		{
-		e.setQuitMessage(ChatColor.DARK_GREEN + (this.settings.getConfig().getString("leftmessage")).replace("%p%", p.getName()));
-		}
+			e.setQuitMessage(ChatColor.DARK_GREEN + (this.settings.getConfig().getString("leftmessage")).replace("%p%", p.getName()));
 	}
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	  {
-	    if (label.equalsIgnoreCase("ln")) 
-	    {
+	    if (label.equalsIgnoreCase("ln")) {
 	      if (args.length == 1) {
-	      if (args[0].equalsIgnoreCase("reload")) 
-	        {
-	        this.settings.reloadConfig();
-	        sender.sendMessage("§7 config reloaded!");
-	        }
-	        else 
-	        {	         
-	        sender.sendMessage("§cUsage: /ln <Reload>");
+	        if (args[0].equalsIgnoreCase("reload")) {
+	          this.settings.reloadConfig();
+	          sender.sendMessage("§7 Config reloaded!");
+	        } else {
+	          sender.sendMessage("§cUsage: /ln <Reload>");
 	        }
 	      }
 	      else sender.sendMessage("§cUsage: /ln <Reload>");
+
 	    }
 
 	    return true;
 	  }
+	   
+	   private void loadConfig() {
+		    getConfig().options().header("|----------------|\n| Login Nachtricht |\n|----------------|");
+		    getConfig().options().copyDefaults(true);
 
+
+		    String Update = "ln.AutoUpdate.Enabled?";
+		    getConfig().addDefault(Update, Boolean.valueOf(false));
+		    getConfig().addDefault("message", "Willkommen %p% , Viel Spaß beim Spielen auf.");
+		    getConfig().addDefault("leftmessage", "%p% hat den Server  verlassen. ");
+
+		    saveConfig();
+		  }
 }
